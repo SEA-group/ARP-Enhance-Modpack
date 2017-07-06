db9383192f8609f534f7dfe4dc1d28c4630588fe
AstreTunes <mebius.uranus@gmail.com>
2017-07-07 00:14:55 +0200

    v1.0.0

---
+++ PnFMods/SEA_ARP-Enhance-Pack/Main.py
@@ -0,0 +1,13 @@
+API_VERSION = 'API_v1.0'
+
+contentSdk.registerShipMod('JSC708_ARP_Takao')
+
+contentSdk.registerShipMod('JSB705_Kongou')
+contentSdk.registerShipMod('JSB706_Kirishima')
+contentSdk.registerShipMod('JSB707_Haruna')
+contentSdk.registerShipMod('JSB708_Hiei_Arpeggio')
+
+contentSdk.registerShipMod('JSC705_Myoko')
+contentSdk.registerShipMod('JSC707_Ashigara')
+contentSdk.registerShipMod('JSC709_Haguro')
+contentSdk.registerShipMod('JSC737_Nachi')
